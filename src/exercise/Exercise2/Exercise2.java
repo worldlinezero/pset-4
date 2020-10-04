6f909187377bd4ad7769f3beca55423b9f6ff4e7
Aidan Bennett <abennett@ucvts.org>
2020-10-04 19:16:06 -0400

    forget to change and add exercise 2, do both of those things

--- src/exercise/Exercise2/Exercise2.java
+++ src/exercise/Exercise2/Exercise2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Exercise2 {
     public static void main(String[] args) {
         Scanner distances = new Scanner(System.in);
-        System.out.println("Inches :");
+        System.out.print("Inches :");
         double amount = distances.nextDouble();
         double miles = amount / 63360;
         miles = (int) miles;
